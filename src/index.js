cf3ed2f45ce20f2b47f618e1d3e697eb1f6920e0
sachin <44335319+sachinmaleesha@users.noreply.github.com>
2022-09-01 07:36:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  app = express()
+    app.use(express.static('reactFolderName/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'reactFolderName', 'build', 'index.html')));
     <App />
   </React.StrictMode>
 );
